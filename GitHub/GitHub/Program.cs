5b7b7fa95cb554b3961043cba9b5d314c0e637ed
APAC\jj30089 <jj30089@imcap.ap.ssmb.com>
2018-07-31 14:06:30 +0800

    Init

---
+++ GitHub/GitHub/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1 + 4;
+        }
+    }
+}
